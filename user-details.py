062c8643e3627009f0ea3ed5d5fb9331f2eb24c6
grovesys <itsjohngroves@gmail.com>
2015-05-17 10:21:01 +0100

    Create user-details.py

---
+++ user-details.py
@@ -0,0 +1,9 @@
+name = input("What is your name: ")
+age = int(input("How old are you: "))
+location = input("Where do you live: ")
+telephone = input("What is your telephone number: ")
+
+print ("Hi", name, "You are",age, "and you live in",location,"and we can call you on",telephone,".")
+
+# Things to address:
+# Whitespace before and after variables, what if I don't want the space?
